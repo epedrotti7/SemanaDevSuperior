9c89cac08bd0e275ad87a6acadd9154a986cebf1
Eliton Pedrotti <pedrottieliton777@gmail.com>
2020-09-18 19:08:47 -0300

    GET records

--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -3,6 +3,8 @@ package com.devsuperior.dspesquisa.services;
 import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,5 +39,11 @@ public class RecordService {
 		entity = repository.save(entity);
 		
 		return new RecordDTO(entity);
+	}
+
+		@Transactional(readOnly = true)
+	public Page<RecordDTO> findByMoments(Instant minDate, Instant maxDate, PageRequest pageRequest) {
+		// TODO Auto-generated method stub
+		return repository.findByMoments(minDate, maxDate, pageRequest).map(x -> new RecordDTO(x));
 	}
 }
