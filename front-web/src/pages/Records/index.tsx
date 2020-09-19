c755ea9f42f898db0c068deb93e1137b669f8194
Eliton Pedrotti <pedrottieliton777@gmail.com>
2020-09-19 00:32:31 -0300

    criando páginas de listagem dos registros

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './styles.css'
+import { RecordsResponse } from './types'
+import {formatDate} from './helpers'
+import Pagination from './Pagination'
 
-const Records = () =>(
-    <h1>Records</h1>
-)
+const BASE_URL = "http://localhost:8080"
+
+const Records = () =>{
+    const [recordsResponse, setRecordsResponse] = useState<RecordsResponse>()
+    const [activePage, setActivePage] = useState(0)
+
+
+    useEffect(()=> {
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+        .then(response => setRecordsResponse(response.data))
+    }, [activePage])
+
+    const handlePageChange = (index: number) =>{
+        setActivePage(index)
+    }
+
+    return (
+        <div className="page-container">
+        <table className="records-table" cellPadding="0" cellSpacing="0">
+            <thead>
+                <tr>
+                    <th>INSTANTE</th>
+                    <th>NOME</th>
+                    <th>IDADE</th>
+                    <th>PLATAFORMA</th>
+                    <th>GÊNERO</th>
+                    <th>TITULO DO GAME</th>
+                </tr>
+            </thead>
+            <tbody>
+                {recordsResponse?.content.map(record => (
+                    <tr key={record.id}>
+                        <td>{formatDate(record.moment)}</td>
+                        <td>{record.name}</td>
+                        <td>{record.age}</td>
+                        <td className="text-secondary">{record.gamePlatform}</td>
+                        <td>{record.genreName}</td>
+                        <td className="text-primary">{record.gameTitle}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <Pagination 
+        activePage={activePage}
+        goToPage={handlePageChange}
+        totalPages={recordsResponse?.totalPages}
+        />
+    </div>
+    )
+}
 
 export default Records
---
+++ front-web/src/pages/Records/types.ts
@@ -0,0 +1,19 @@
+
+export type RecordsResponse = {
+    content: RecordItem[]
+    totalPages: number
+}
+
+
+export type RecordItem = {
+        id: number
+        moment: string
+        name: string 
+        age: number 
+        gameTitle: string 
+        gamePlatform: Platform
+        genreName: string
+}
+
+
+export type Platform = 'XBOX' | 'PC' | 'PLAYSTATION'
