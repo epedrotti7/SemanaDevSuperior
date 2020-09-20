207270c6660c1f286ab75599d4af864983a9413a
Eliton Pedrotti <pedrottieliton777@gmail.com>
2020-09-20 12:52:57 -0300

    finalizando app react native

---
+++ front-mobile/src/pages/CreateRecord/types.tsx
@@ -0,0 +1,8 @@
+export type GamePlatform = 'XBOX' | 'PC' | 'PLAYSTATION'
+export type Game = {
+    id: number
+    title: string
+    platform: GamePlatform
+    label: string
+    value: number
+}
